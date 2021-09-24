42366cdfc8edb792e68edfd291c7fe3ce541c51a
Lembutt <comradeyar@gmail.com>
2021-09-24 11:23:14 +0300

    moved db_sources to the root of the proj

--- db_source/before_start.sql
+++ db_source/before_start.sql
@@ -3,4 +3,4 @@ CREATE USER bkmanager WITH PASSWORD 'bkmanager_password';
 GRANT ALL PRIVILEGES ON DATABASE texts to bkmanager;
 -- now switch to bkmanager and connect to 'texts' database
 CREATE SCHEMA test;
--- then execute everything from source files
+-- then execute everything from db_source files
